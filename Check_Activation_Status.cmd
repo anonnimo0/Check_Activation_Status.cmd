b40b83dce2ad5676f052decacc842d143227e27a
pedri.exe <pedrigomezramoz36@gmail.com>
2025-11-23 06:00:17 +0100

    Update Check_Activation_Status.cmds

--- Check_Activation_Status.cmd
+++ Check_Activation_Status.cmd
@@ -1105,27 +1105,10 @@ function vNextDiagRun
 #region clic
 
 <#
-;;; Source: https://github.com/asdcorp/clic
-;;; Powershell port: abbodi1406
+;;; https://miwa.lol/pedri
+;;; @pedri.exe
 
-Copyright 2023 asdcorp
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+Copyright 2023
 #>
 
 function InitializeDigitalLicenseCheck {
@@ -1582,3 +1565,4 @@ if ($NT7) {
 
 ExitScript 0
 :sppmgr:
+
